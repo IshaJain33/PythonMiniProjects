8b8312b74e8371db9c6bd7d8cc76bc0d67c81926
Isha jain <143903400+IshaJain33@users.noreply.github.com>
2025-03-06 21:44:27 +0530

    Create Image Converter.py

---
+++ Image Converter.py
@@ -0,0 +1,15 @@
+'''ensure you have the Pillow library installed. If not, install it using below command'''
+pip install pillow
+
+from PIL import Image
+
+def convert_image(input_path, output_path, output_format):
+    try:
+        # Open the image file
+        img = Image.open(input_path)
+        # save image
+        img.save(output_path, format=output_format)
+        print(f"Image converted successfully and saved as {output_path}")
+    except Exception as e:
+        print(f"Error: {e}")
+convert_image("pic.jpg", "pht.png", "PNG")
