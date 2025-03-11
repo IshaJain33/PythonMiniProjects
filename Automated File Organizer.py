f5448dde571cf29dc3294e818369ea05f35cdee5
Isha jain <143903400+IshaJain33@users.noreply.github.com>
2025-03-11 21:43:03 +0530

    Create Automated File Organizer.py

---
+++ Automated File Organizer.py
@@ -0,0 +1,20 @@
+import os
+import shutil
+
+source_folder = "C:/Users/User/Downloads"
+destinations = {
+    "Images": [".png", ".jpg", ".jpeg", ".gif"],
+    "Documents": [".pdf", ".docx", ".txt"],
+    "Videos": [".mp4", ".mkv"],
+}
+
+for file in os.listdir(source_folder):
+    file_path = os.path.join(source_folder, file)
+    if os.path.isfile(file_path):
+        for folder, extensions in destinations.items():
+            if file.endswith(tuple(extensions)):
+                folder_path = os.path.join(source_folder, folder)
+                os.makedirs(folder_path, exist_ok=True)
+                shutil.move(file_path, folder_path)
+
+print("Files organized successfully!")
