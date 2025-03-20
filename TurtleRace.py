fd72543f3c1af2d7272de02fe8819af1fc2e2645
Isha jain <143903400+IshaJain33@users.noreply.github.com>
2025-03-20 20:53:16 +0530

    Update TurtleRace.py

--- TurtleRace.py
+++ TurtleRace.py
@@ -2,7 +2,7 @@ import turtle
 import random
 
 screen = turtle.Screen()
-screen.title("Turtle Race 🏁")
+screen.title("Turtle Race")
 screen.bgcolor("lightblue")
 track = turtle.Turtle()
 track.speed(0)
@@ -18,13 +18,10 @@ for i in range(6):
     track.left(90)
     track.forward(50)
     track.left(90)
-
 track.hideturtle()
 
-# Create turtles
 colors = ["red", "blue", "green", "purple", "orange"]
 turtles = []
-
 for i in range(5):
     racer = turtle.Turtle()
     racer.color(colors[i])
@@ -33,14 +30,11 @@ for i in range(5):
     racer.goto(-200, 75 - (i * 50))
     racer.pendown()
     turtles.append(racer)
-
-# Start race
 race_on = True
 while race_on:
     for racer in turtles:
         move = random.randint(1, 5)
         racer.forward(move)
-
         if racer.xcor() >= 200:  # Finish line
             race_on = False
             winner = racer.color()[0]  # Get winner color
